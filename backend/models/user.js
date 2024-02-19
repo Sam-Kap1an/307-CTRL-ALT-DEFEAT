f1f61fa2fbbe18a1c1fd644e8aa44a7baa74ad71
Sohini-pillay <sohinispillay@gmail.com>
2024-02-18 20:50:05 -0800

    schemas tentatively added

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import Location from "./location";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    locations: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Location",
+        required: false,
+      },
+    ],
+  },
+  { collection: "users" }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
