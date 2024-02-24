229f9de86443ddaa3822907833df416a458514d0
Yanna <shirokovayanna@gmail.com>
2024-02-24 08:58:58 -0800

    inventory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './login/login'; 
 import Create from './create/create'; 
 import Home from "./components/home/Home";
+import Inventory from "./inventory/Inventory"
+
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Inventory />} />
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Create />} />
