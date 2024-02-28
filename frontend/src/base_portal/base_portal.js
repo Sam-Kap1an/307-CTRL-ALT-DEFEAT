afd163a55e71f4472bc2f67ca507fec0e7e34596
sfkaplan <sfkaplan@calpoly.edu>
2024-02-27 16:31:17 -0800

    BASE base_portal

--- frontend/src/base_portal/base_portal.js
+++ frontend/src/base_portal/base_portal.js
@@ -35,45 +35,42 @@ function Base_portal() {
     navigate('/');
   };
 
-  const [newProduct, setNewProduct] = useState({
+  const [newLocation, setNewLocation] = useState({
     name: '',
-    quantity: '',
-    description: '',
-    minimumThreshold: '',
+    catagories: '',
   });
 
-  const [inventory, setInventory] = useState([]);
+  const [location, setLocation] = useState([]);
   const [editedItemId, setEditedItemId] = useState(null);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [filterOption, setFilterOption] = useState('All');
+
 
   useEffect(() => {
-    fetchInventory();
+    fetchLocation();
   }, []);
 
-  const fetchInventory = () => {
-    fetch('http://localhost:8000/inventory')
+  const fetchLocation = () => {
+    fetch('http://localhost:8000/location')
       .then((response) => response.json())
-      .then((data) => setInventory(data))
-      .catch((error) => console.error('Error fetching inventory:', error));
+      .then((data) => setLocation(data))
+      .catch((error) => console.error('Error fetching location:', error));
   };
 
   const handleAddNewClick = () => {
     onOpen(); // Open the modal when Add New button is clicked
   };
 
-  const handleAddNewProduct = () => {
-    fetch('http://localhost:8000/inventory', {
+  const handleAddNewLoction = () => {
+    fetch('http://localhost:8000/location', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(newProduct),
+      body: JSON.stringify(newLocation),
     })
       .then((response) => response.json())
       .then((data) => {
-        setInventory([...inventory, data]);
-        setNewProduct({ name: '', quantity: '', description: '', minimumThreshold: '' });
+        setLocation([...location, data]);
+        setNewLocation({ name: '', catagories: '' });
         onClose(); // Close the modal after adding a new product
       })
       .catch((error) => console.error('Error adding new product:', error));
@@ -100,9 +97,7 @@ function Base_portal() {
   const handleSaveEdit = (itemId) => {
     const editedData = {
       name: document.getElementById(`name-${itemId}`).value,
-      quantity: document.getElementById(`quantity-${itemId}`).value,
-      description: document.getElementById(`description-${itemId}`).value,
-      minimumThreshold: document.getElementById(`minimumThreshold-${itemId}`).value,
+      catagories: document.getElementById(`catagories-${itemId}`).value,
     };
 
     fetch(`http://localhost:8000/inventory/${itemId}`, {
@@ -115,11 +110,11 @@ function Base_portal() {
       .then((response) => response.json())
       .then(() => {
         console.log('Item updated successfully');
-        setInventory((prevInventory) => {
-          const updatedInventory = prevInventory.map((item) =>
+        setLocation((prevLocation) => {
+          const updatedLocation = prevLocation.map((item) =>
             item._id === itemId ? { ...item, ...editedData } : item
           );
-          return updatedInventory;
+          return updatedLocation;
         });
       })
       .catch((error) => console.error('Error updating item:', error))
@@ -129,15 +124,12 @@ function Base_portal() {
   };
 
   const handleInputChange = (e, itemId, field) => {
-    const updatedInventory = inventory.map((item) =>
+    const updatedLocation = location.map((item) =>
       item._id === itemId ? { ...item, [field]: e.target.value } : item
     );
-    setInventory(updatedInventory);
+    setLocation(updatedLocation);
   };
 
-  const filteredInventory = inventory.filter((item) =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <Box className="inventory-container">
@@ -149,167 +141,23 @@ function Base_portal() {
         <Text fontSize="2xl" fontWeight="bold">
           Inventory
         </Text>
-        <Button onClick={handleBackClick} colorScheme="teal" variant="outline">
+        <Button onClick={handleAddNewClick} colorScheme="Red" variant="outline">
           Back
         </Button>
       </Box>
-
-      <Box className="search-filter-buttons">
-        <Input
-          type="text"
-          placeholder="Search Product"
-          className="search-input"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-
-        <Select
-          value={filterOption}
-          onChange={(e) => setFilterOption(e.target.value)}
-          
-          className="filter-input"
-        >
-          <option value="All">All</option>
-          <option value="Low">Low</option>
-          <option value="High">High</option>
-        </Select>
-
-        <Box className="blue-buttons">
-          {/* Change the Add New button to open the modal */}
-          <Button onClick={handleAddNewClick} colorScheme="blue">
-            Add New
-          </Button>
-        </Box>
-      </Box>
-
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>Product</Th>
-            <Th>Quantity</Th>
-            <Th>Description</Th>
-            <Th>Minimum Threshold</Th>
-            <Th>Edit</Th>
-            <Th>Delete</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {filteredInventory.map((item) => (
-            <Tr
-              key={item._id}
-              style={{
-                backgroundColor:
-                  parseFloat(item.minimumThreshold) > parseFloat(item.quantity)
-                    ? 'rgba(255, 0, 0, 0.1)' // Red with transparency
-                    : 'rgba(0, 255, 0, 0.1)', // Green with transparency
-              }}
-              display={
-                filterOption === 'All' ||
-                (filterOption === 'Low' &&
-                  parseFloat(item.minimumThreshold) > parseFloat(item.quantity)) ||
-                (filterOption === 'High' &&
-                  parseFloat(item.minimumThreshold) <= parseFloat(item.quantity))
-                  ? 'table-row'
-                  : 'none'
-              }
-            >
-              <Td>
-                {editedItemId === item._id ? (
-                  <Input
-                    type="text"
-                    id={`name-${item._id}`}
-                    value={item.name}
-                    onChange={(e) => handleInputChange(e, item._id, 'name')}
-                  />
-                ) : (
-                  item.name
-                )}
-              </Td>
-              <Td>
-                {editedItemId === item._id ? (
-                  <Input
-                    type="text"
-                    id={`quantity-${item._id}`}
-                    value={item.quantity}
-                    onChange={(e) => handleInputChange(e, item._id, 'quantity')}
-                  />
-                ) : (
-                  item.quantity
-                )}
-              </Td>
-              <Td>
-                {editedItemId === item._id ? (
-                  <Input
-                    type="text"
-                    id={`description-${item._id}`}
-                    value={item.description}
-                    onChange={(e) => handleInputChange(e, item._id, 'description')}
-                  />
-                ) : (
-                  item.description
-                )}
-              </Td>
-              <Td>
-                {editedItemId === item._id ? (
-                  <Input
-                    type="text"
-                    id={`minimumThreshold-${item._id}`}
-                    value={item.minimumThreshold}
-                    onChange={(e) => handleInputChange(e, item._id, 'minimumThreshold')}
-                  />
-                ) : (
-                  item.minimumThreshold
-                )}
-              </Td>
-              <Td>
-                {editedItemId === item._id ? (
-                  <Button onClick={() => handleSaveEdit(item._id)} colorScheme="teal">
-                    Save
-                  </Button>
-                ) : (
-                  <Button onClick={() => handleEditClick(item._id)} colorScheme="teal">
-                    Edit
-                  </Button>
-                )}
-              </Td>
-              <Td>
-                <Button
-                  onClick={() => handleDeleteClick(item._id)}
-                  colorScheme="red"
-                  variant="outline"
-                >
-                  🗑️
-                </Button>
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-
+      
       {/* Modal for adding a new product */}
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Add New Product</ModalHeader>
+          <ModalHeader>Add New Location</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <Input
               type="text"
               placeholder="Product Name"
-              value={newProduct.name}
+              value={newLocation.name}
               onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
-            />
-            <Input
-              type="text"
-              placeholder="Quantity"
-              value={newProduct.quantity}
-              onChange={(e) => setNewProduct({ ...newProduct, quantity: e.target.value })}
-            />
-            <Input
-              type="text"
-              placeholder="Description"
-              value={newProduct.description}
-              onChange={(e) => setNewProduct({ ...newProduct, description: e.target.value })}
             />
             <Input
               type="text"
@@ -328,6 +176,31 @@ function Base_portal() {
           </ModalFooter>
         </ModalContent>
       </Modal>
+
+      <Table>
+        <Thead>
+          <Tr>
+            {/*all of the groups*/}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {location.map((item) => (
+            <Tr
+              key={item._id}
+              display={
+                filterOption === 'All' ||
+                (filterOption === 'Low' &&
+                  parseFloat(item.minimumThreshold) > parseFloat(item.quantity)) ||
+                (filterOption === 'High' &&
+                  parseFloat(item.minimumThreshold) <= parseFloat(item.quantity))
+                  ? 'table-row'
+                  : 'none'
+              }
+            >
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </Box>
   );
 }
