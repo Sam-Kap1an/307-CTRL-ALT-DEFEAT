0f627ab3278879db6978e32bf0a0fcf5438a2934
sfkaplan <sfkaplan@calpoly.edu>
2024-02-29 14:17:20 -0800

    FUCK Catagories

--- frontend/src/base_portal/base_portal.js
+++ frontend/src/base_portal/base_portal.js
@@ -33,8 +33,7 @@ function Base_portal() {
   });
 
   const [location, setLocation] = useState([]);
-  const [editedLocationId, setLocationId] = useState(null);
-
+  const [category, setCatagory] = useState([]);
 
   useEffect(() => {
     fetchLocation();
@@ -46,6 +45,20 @@ function Base_portal() {
       .then((data) => setLocation(data))
       .catch((error) => console.error('Error fetching location:', error));
   };
+
+  const fetchCategory = (Catagory) => {
+    fetch(`http://localhost:8000/catagory/${Catagory}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(category),
+    })
+    .then((response) => response.json())
+    .then((data) => setCatagory(data))
+    .catch((error) => console.error('Error fetching Catagory:', error));
+  };
+
 
   const handleAddNewClick = () => {
     onOpen(); // Open the modal when Add New button is clicked
@@ -62,71 +75,15 @@ function Base_portal() {
       .then((response) => response.json())
       .then((data) => {
         setLocation([...location, data]);
-        setNewLocation({ name: '', catagories: '' });
+        setNewLocation({ name: '', catagories: fetchCategory('') });
         onClose(); // Close the modal after adding a new product
       })
       .catch((error) => console.error('Error adding new product:', error));
   };
 
-  const handleDeleteClick = (LocationId) => {
-    fetch(`http://localhost:8000/location/${LocationId}`, {
-      method: 'DELETE',
-    })
-      .then((response) => {
-        if (response.ok) {
-          fetchLocation();
-        } else {
-          console.error('Error deleting item');
-        }
-      })
-      .catch((error) => console.error('Error deleting item:', error));
-  };
-
-  const handleEditClick = (LocationId) => {
-    setLocationId(LocationId);
-  };
-
-  const handleSaveEdit = (LocationId) => {
-    const editedData = {
-      name: document.getElementById(`name-${LocationId}`).value,
-      catagories: document.getElementById(`catagories-${LocationId}`).value,
-    };
-
-    const handleInputChange = (e, LocationId, field) => {
-      const updatedLocation = location.map((item) =>
-        item._id === LocationId ? { ...item, [field]: e.target.value } : item
-      );
-      setLocation(updatedLocation);
-    };
-  
-
-    
-    fetch(`http://localhost:8000/location/${LocationId}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(editedData),
-    })
-      .then((response) => response.json())
-      .then(() => {
-        console.log('Item updated successfully');
-        setLocation((prevLocation) => {
-          const updatedLocation = prevLocation.map((item) =>
-            item._id === LocationId ? { ...item, ...editedData } : item
-          );
-          return updatedLocation;
-        });
-      })
-      .catch((error) => console.error('Error updating item:', error))
-      .finally(() => {
-        setLocationId(null);
-      });
-  };
-
   
   return (
-    <Box className="inventory-container">
+    <Box className="Location-container">
       <Box id="sortify-text" onClick={handleSortifyClick}>
         <Text fontSize="90px" fontWeight="bold" letterSpacing="20px">
             <span style={{ color: "#D47697" }}>SOR</span>
@@ -154,7 +111,7 @@ function Base_portal() {
           <ModalBody>
             <Input
               type="text"
-              placeholder="Product Name"
+              placeholder="Location Name"
               value={newLocation.name}
               onChange={(e) => setNewLocation({ ...newLocation, name: e.target.value })}
             />
@@ -166,10 +123,10 @@ function Base_portal() {
             />
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button colorScheme="pink" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button colorScheme="teal" onClick={handleAddNewLoction}>
+            <Button colorScheme="pink" onClick={handleAddNewLoction}>
               Add
             </Button>
           </ModalFooter>
