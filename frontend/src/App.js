cb324d2e355b6a08d031dbae912e35a1f21735f0
sfkaplan <sfkaplan@calpoly.edu>
2024-02-29 15:22:06 -0800

    fixed merge?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./login/login";
 import Create from "./create/create";
 import Home from "./components/home/Home";
-import Inventory from "./inventory/Inventory";
+import Inventory from "./inventory/Inventory"
+import Base_portal from './base_portal/base_portal';
+
+
 
 function App() {
   return (
