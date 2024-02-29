fd1f74d14ad68e23f1add690d89d8890572526cd
sfkaplan <sfkaplan@calpoly.edu>
2024-02-29 15:15:57 -0800

    Routes for loc + Loc FE

---
+++ backend/routes/location-services.js
@@ -0,0 +1,50 @@
+import Inventory from "../models/inventory.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+mongoose.set("debug", true);
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+function getInventory(searchQuery) {
+  // Use a regex to perform case-insensitive search
+  const regex = new RegExp(searchQuery, "i");
+  return Inventory.find({ name: regex });
+}
+
+function addItemToInventory(item) {
+  const newItem = new Inventory(item);
+  return newItem.save();
+}
+
+function deleteItemFromInventory(itemId) {
+  return Inventory.findByIdAndDelete(itemId);
+}
+
+function searchInventory(searchTerm) {
+  return Inventory.find({ name: { $regex: new RegExp(searchTerm, "i") } });
+}
+
+function updateItemInInventory(itemId, updatedData) {
+  return Inventory.findByIdAndUpdate(itemId, updatedData, { new: true });
+}
+
+export default {
+  getInventory,
+  addItemToInventory,
+  deleteItemFromInventory,
+  searchInventory,
+  updateItemInInventory,
+};
